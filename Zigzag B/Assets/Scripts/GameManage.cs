3c720ed6b412bfaadb7971486ad9a277850e81cd
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2018-03-04 19:45:21 +0530

    ZigZag-B 3D Game

---
+++ Zigzag B/Assets/Scripts/GameManage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManage : MonoBehaviour {
+
+	public static GameManage instance;
+	public bool gameOver;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame() {
+		UiManager.instance.GameStart ();
+		ScoreManager.instance.startScore ();
+
+		GameObject.Find ("PlatformSpawners").GetComponent<PlatformSpawners> ().StartSpawningPlatforms ();
+
+	}
+
+	public void GameOver() {
+		UiManager.instance.GameOver ();
+		ScoreManager.instance.stopScore ();
+		gameOver = true;
+	}
+}
---
+++ Zigzag B/Assets/Scripts/UiManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+	public static UiManager instance;
+
+	public GameObject zigzagPanel;
+	public GameObject gameOverPanel;
+	public GameObject tapText;
+	public Text score;
+	public Text highscore1;
+	public Text highscore2;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		highscore1.text = "High Score: " + PlayerPrefs.GetInt ("highScore");//.ToString ();
+	}
+
+	public void GameStart() {
+		highscore1.text = PlayerPrefs.GetInt ("highScore").ToString();
+		tapText.SetActive (false);
+		zigzagPanel.GetComponent<Animator>().Play("panelUp");
+	}
+
+	public void GameOver() {
+		score.text = PlayerPrefs.GetInt ("score").ToString();
+		highscore2.text = PlayerPrefs.GetInt ("highScore").ToString();
+		gameOverPanel.SetActive (true);
+	}
+
+	public void Reset() {
+		SceneManager.LoadScene (0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
